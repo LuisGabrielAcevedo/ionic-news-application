182f486e6a682d3c8f3311be79b67cd56052be1c
Luis gabriel <luisgabriel_ace@hotmail.com>
2019-11-14 22:48:24 -0300

    pugins

--- ionic-news-application/src/app/components/news-card/news-card.component.ts
+++ ionic-news-application/src/app/components/news-card/news-card.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from "@angular/core";
 import { Article } from "src/app/interfaces/interfaces";
+import { InAppBrowser } from "@ionic-native/in-app-browser/ngx";
 
 @Component({
   selector: "app-news-card",
@@ -7,9 +8,13 @@ import { Article } from "src/app/interfaces/interfaces";
   styleUrls: ["./news-card.component.scss"]
 })
 export class NewsCardComponent implements OnInit {
-  @Input() public new: Article;
+  @Input() public news: Article;
   @Input() public index: number;
-  constructor() {}
+  constructor(private iab: InAppBrowser) {}
 
   ngOnInit() {}
+
+  public openNews() {
+    const browser = this.iab.create(this.news.url, "_system");
+  }
 }
