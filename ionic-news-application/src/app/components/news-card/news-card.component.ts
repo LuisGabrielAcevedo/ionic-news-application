e72748e902d6fa51bf44625e27de81cab90d8395
Luis gabriel <luisgabriel_ace@hotmail.com>
2019-11-15 06:28:37 -0300

    action-sheet

--- ionic-news-application/src/app/components/news-card/news-card.component.ts
+++ ionic-news-application/src/app/components/news-card/news-card.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input } from "@angular/core";
 import { Article } from "src/app/interfaces/interfaces";
 import { InAppBrowser } from "@ionic-native/in-app-browser/ngx";
+import { ActionSheetController } from "@ionic/angular";
 
 @Component({
   selector: "app-news-card",
@@ -10,11 +11,47 @@ import { InAppBrowser } from "@ionic-native/in-app-browser/ngx";
 export class NewsCardComponent implements OnInit {
   @Input() public news: Article;
   @Input() public index: number;
-  constructor(private iab: InAppBrowser) {}
+  constructor(
+    private iab: InAppBrowser,
+    public actionSheetController: ActionSheetController
+  ) {}
 
   ngOnInit() {}
 
   public openNews() {
     const browser = this.iab.create(this.news.url, "_system");
+  }
+
+  public async openMenu() {
+    const actionSheet = await this.actionSheetController.create({
+      buttons: [
+        {
+          text: "Compartir",
+          icon: "share",
+          cssClass: "action-dark",
+          handler: () => {
+            console.log("Share clicked");
+          }
+        },
+        {
+          text: "Favorito",
+          icon: "star",
+          cssClass: "action-dark",
+          handler: () => {
+            console.log("Favorite clicked");
+          }
+        },
+        {
+          text: "Cancelar",
+          icon: "close",
+          role: "cancel",
+          cssClass: "action-dark",
+          handler: () => {
+            console.log("Cancel clicked");
+          }
+        }
+      ]
+    });
+    await actionSheet.present();
   }
 }
